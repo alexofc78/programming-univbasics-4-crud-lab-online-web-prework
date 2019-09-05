3e2c162126a8346685216248d77a14152b6e373f
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-05 17:20:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["banana", "chocolate", "rice", "bread"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  element = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  element = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  index_number = 2 
 end
 
 def retrieve_first_element_from_array(array)
-  
+  index_number = 0
 end
 
 def retrieve_last_element_from_array(array)
-  
+  index_number = -1
 end
 
 def update_element_from_index(array, index_number, element)
